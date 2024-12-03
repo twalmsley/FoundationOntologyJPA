31ae058ff9131b14a6d90105c1849dee8b8c3bd3
Tony <tony@aosd.co.uk>
2024-12-03 08:21:46 +0000

    Added a test for DNA persistence.

--- src/main/java/uk/co/aosd/onto/jpa/DNAJpa.java
+++ src/main/java/uk/co/aosd/onto/jpa/DNAJpa.java
@@ -1,6 +1,8 @@
 package uk.co.aosd.onto.jpa;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,7 +18,10 @@ import uk.co.aosd.onto.biological.DNA;
 @AllArgsConstructor
 @NoArgsConstructor
 public class DNAJpa implements DNA {
+    @Id
     private String identifier;
+
+    @Column(name = "DNA_VALUE")
     private String dna;
 
 }
