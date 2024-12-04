b5e0c6e86406a8dd92dd06f5f29bc6305c328c4d
Tony <tony@aosd.co.uk>
2024-12-04 08:48:00 +0000

    Added a test for ScalarPropertyJpa persistence.

--- src/main/java/uk/co/aosd/onto/jpa/ScalarPropertyJpa.java
+++ src/main/java/uk/co/aosd/onto/jpa/ScalarPropertyJpa.java
@@ -2,14 +2,19 @@ package uk.co.aosd.onto.jpa;
 
 import java.util.Set;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Convert;
 import jakarta.persistence.Entity;
-import lombok.AllArgsConstructor;
+import jakarta.persistence.OneToMany;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Target;
 import uk.co.aosd.onto.foundation.ScalarProperty;
 import uk.co.aosd.onto.foundation.ScalarValue;
 import uk.co.aosd.onto.foundation.UniquelyIdentifiable;
 import uk.co.aosd.onto.foundation.Unit;
+import uk.co.aosd.onto.jpa.converters.ScalarValueConverter;
 
 /**
  * An implementation of the ScalarProperty interface.
@@ -18,10 +23,24 @@ import uk.co.aosd.onto.foundation.Unit;
  */
 @Entity
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
-public class ScalarPropertyJpa<T extends UniquelyIdentifiable, U extends Number, V extends Unit> implements ScalarProperty<T, U, V> {
-    private String identifier;
+@EqualsAndHashCode(callSuper = true)
+public class ScalarPropertyJpa<T extends UniquelyIdentifiable, U extends Number, V extends Unit> extends UniquelyIdentifiableJpa
+    implements ScalarProperty<T, U, V> {
+
+    @Convert(converter = ScalarValueConverter.class)
+    @Target(ScalarValueJpa.class)
     private ScalarValue<U, V> property;
+
+    @OneToMany(targetEntity = UniquelyIdentifiableJpa.class, cascade = { CascadeType.PERSIST, CascadeType.MERGE })
     private Set<T> members;
+
+    /**
+     * Constructor.
+     */
+    public ScalarPropertyJpa(final String identifier, final ScalarValue<U, V> property, final Set<T> members) {
+        super(identifier);
+        this.property = property;
+        this.members = members;
+    }
 }
---
+++ src/test/java/uk/co/aosd/onto/jpa/ScalarPropertyTest.java
@@ -0,0 +1,79 @@
+package uk.co.aosd.onto.jpa;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.time.Instant;
+import java.util.Set;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import uk.co.aosd.onto.jpa.events.BuiltJpa;
+import uk.co.aosd.onto.jpa.events.ScrappedJpa;
+import uk.co.aosd.onto.units.Units;
+
+/**
+ * Test persistence for ScalarPropertyJpa.
+ */
+public class ScalarPropertyTest {
+    private static EntityManagerFactory emf;
+    private EntityManager em;
+
+    @BeforeAll
+    public static void setUpClass() {
+        emf = Persistence.createEntityManagerFactory("h2-persistence-unit");
+    }
+
+    @BeforeEach
+    public void setUp() {
+        em = emf.createEntityManager();
+    }
+
+    /**
+     * Tear down.
+     */
+    @AfterEach
+    public void tearDown() {
+        if (em != null) {
+            em.close();
+        }
+    }
+
+    /**
+     * Tear down.
+     */
+    @AfterAll
+    public static void tearDownClass() {
+        if (emf != null) {
+            emf.close();
+        }
+    }
+
+    @Test
+    public void test() {
+        final Instant to = Instant.now();
+        final Instant from = to.minusSeconds(60);
+        final var beginning = new BuiltJpa("built031", from, to);
+        final var ending = new ScrappedJpa("scrapped03", from, to);
+        final var ind = new IndividualJpa("ind03", beginning, ending);
+        final var scalarValue = new ScalarValueJpa<>(1, Units.METERS);
+        final var entity = new ScalarPropertyJpa<>("id03", scalarValue, Set.of(ind));
+
+        em.getTransaction().begin();
+        em.persist(entity);
+        em.getTransaction().commit();
+
+        final var found = em.find(ScalarPropertyJpa.class, entity.getIdentifier());
+        assertNotNull(found);
+        assertEquals(entity.getIdentifier(), found.getIdentifier());
+        assertTrue(entity.getMembers().contains(ind));
+        assertEquals(entity.getProperty(), found.getProperty());
+    }
+}
